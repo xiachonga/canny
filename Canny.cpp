9d8627c65d2625cc9d6b77cd4029697399e2eb58
xiachong <854478175@qq.com>
2022-01-14 10:18:47 +0800

    create benchmark

--- Canny.cpp
+++ Canny.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 const double  PI = 3.14159265358979323846;
+const int height = 512;
+const int width = 512;
+#define ptr(i) i * width
 
 Mat<uchar> Canny::gaussianFilter(Mat<uchar> &image, double sigma) {
-    int width = image.getWidth();
-    int height = image.getHeight();
     Mat<uchar> mat(width, height);
     //根据sigma确定高斯核的大小
     int len = 1 + 2 * ceil(3 * sigma);
@@ -15,18 +16,21 @@ Mat<uchar> Canny::gaussianFilter(Mat<uchar> &image, double sigma) {
 
     Mat<double> gaussTemplate(len, len);
     double sum = 0.0;
+    uchar* data = mat.getData();
+    uchar* resdata = mat.getData();
+    uchar* gaussData = mat.getData();
     for (int i =0; i < len; ++i) {
         int x = i - len / 2;
         for (int j = 0; j < len; ++j) {
             int y = j - len/2;
-            gaussTemplate.ptr(i)[j] = exp(-(pow(x, 2) + pow(y, 2)) / (2 * pow(sigma, 2)));
-            sum += gaussTemplate.ptr(i)[j];
+            (gaussData + ptr(i))[j] = exp(-(pow(x, 2) + pow(y, 2)) / (2 * pow(sigma, 2)));
+            sum += (gaussData + ptr(i))[j];
         }
     }
 
     for (int i = 0; i < len; ++i) {
         for (int j = 0; j < len; ++j) {
-            gaussTemplate.ptr(i)[j] /= sum;
+            (gaussData + ptr(i))[j] /= sum;
         }
     }
 
@@ -36,9 +40,9 @@ Mat<uchar> Canny::gaussianFilter(Mat<uchar> &image, double sigma) {
                 for (int n = 0; n < len; ++n) {
                     if (i - center + m >= 0 && i - center + m < height &&
                         j - center + n >= 0 && j - center + n < width) {
-                        mat.ptr(i)[j] += (uchar)(gaussTemplate.ptr(m)[n] * image.ptr(i - center + m)[j - center + n]);
+                        (resdata + ptr(i))[j] += (uchar)((gaussData + ptr(m))[n] * (data + ptr(i - center + m))[j - center + n]);
                     } else {
-                        mat.ptr(i)[j] += (uchar)(gaussTemplate.ptr(m)[n] * image.ptr(i)[j]);
+                        (resdata + ptr(i))[j] += (uchar)((gaussData + ptr(m))[n] * (data + ptr(i))[j]);
                     }
                 }
             }
@@ -48,54 +52,54 @@ Mat<uchar> Canny::gaussianFilter(Mat<uchar> &image, double sigma) {
 }
 
 void Canny::sobel(Mat<uchar> &image, Mat<uchar> &grad, Mat<uchar> &theta) {
-    int width = image.getWidth();
-    int height = image.getHeight();
     grad = Mat<uchar>::zeros(width, height);
     theta = Mat<uchar>::zeros(width, height);
-
+    uchar *data = image.getData();
+    uchar *gradData = grad.getData();
+    uchar *thetaData = theta.getData();
     for (int i = 1; i < height - 1; ++i) {
         for (int j = 1; j < width - 1; ++j) {
-            double gradY = double(image.ptr(i - 1)[j - 1] + 2 * image.ptr(i - 1)[j] + image.ptr(i - 1)[j + 1]
-                                  - image.ptr(i + 1)[j - 1] - 2 * image.ptr(i + 1)[j] - image.ptr(i + 1)[j + 1]);
-            double gradX = double(image.ptr(i - 1)[j + 1] + 2 * image.ptr(i)[j + 1] + image.ptr(i + 1)[j + 1]
-                                  - image.ptr(i - 1)[j - 1] - 2 * image.ptr(i)[j - 1] - image.ptr(i + 1)[j - 1]);
-            grad.ptr(i)[j] = (uchar)sqrt(pow(gradX, 2) + pow(gradY, 2));
-            theta.ptr(i)[j] = (uchar)atan(gradY / gradX);
+            double gradY = double((data + ptr(i - 1))[j - 1] + 2 * (data + ptr(i - 1))[j] + (data + ptr(i - 1))[j + 1]
+                                  - (data + ptr(i + 1))[j - 1] - 2 * (data + ptr(i + 1))[j] - (data + ptr(i + 1))[j + 1]);
+            double gradX = double((data + ptr(i - 1))[j + 1] + 2 * (data + ptr(i))[j + 1] + (data + ptr(i + 1))[j + 1]
+                                  - (data + ptr(i - 1))[j - 1] - 2 * (data + ptr(i))[j - 1] - (data + ptr(i + 1))[j - 1]);
+            (gradData + ptr(i))[j] = (uchar)sqrt(pow(gradX, 2) + pow(gradY, 2));
+            (thetaData + ptr(i))[j] = (uchar)atan(gradY / gradX);
         }
     }
 }
 
 void Canny::NMS(Mat<uchar> &grad, Mat<uchar> &theta, Mat<uchar> &dst) {
     dst = grad;
-    int height = grad.getHeight();
-    int width = grad.getWidth();
+    uchar *data = dst.getData();
+    uchar *thetaData = theta.getData();
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
-            double gradValue = double(dst.ptr(i)[j]);
+            double gradValue = double((data + ptr(i))[j]);
             if (gradValue == 0.0) {
                 continue;
             }
-            double currValue = double(theta.ptr(i)[j]);
+            double currValue = double((thetaData + ptr(i))[j]);
             double connectValue1, connectValue2;
             if (currValue >= -PI / 8 && currValue < PI / 8) {
                 //左右
-                connectValue1 = double(dst.ptr(i)[j - 1]);
-                connectValue2 = double(dst.ptr(i)[j + 1]);
+                connectValue1 = double((data + ptr(i))[j - 1]);
+                connectValue2 = double((data + ptr(i))[j + 1]);
             } else if (currValue >= PI / 8 && currValue < 3 * PI / 8) {
                 //右上左下
-                connectValue1 = double(dst.ptr(i + 1)[j - 1]);
-                connectValue2 = double(dst.ptr(i - 1)[j + 1]);
+                connectValue1 = double((data + ptr(i + 1))[j - 1]);
+                connectValue2 = double((data + ptr(i - 1))[j + 1]);
             } else if (currValue >= -3 * PI / 8 && currValue < - PI / 8) {
                 //左上右下
-                connectValue1 = double(dst.ptr(i - 1)[j - 1]);
-                connectValue2 = double(dst.ptr(i + 1)[j + 1]);
+                connectValue1 = double((data + ptr(i - 1))[j - 1]);
+                connectValue2 = double((data + ptr(i + 1))[j + 1]);
             } else {
                 //上下
-                connectValue1 = double(dst.ptr(i - 1)[j]);
-                connectValue2 = double(dst.ptr(i + 1)[j]);
+                connectValue1 = double((data + ptr(i - 1))[j]);
+                connectValue2 = double((data + ptr(i + 1))[j]);
             }
             if (gradValue <= connectValue1 || gradValue <= connectValue2) {
-                dst.ptr(i)[j] = (uchar)0;
+                (data + ptr(i))[j] = (uchar)0;
             }
         }
     }
@@ -103,16 +107,14 @@ void Canny::NMS(Mat<uchar> &grad, Mat<uchar> &theta, Mat<uchar> &dst) {
 }
 void Canny::doubleThreshSelection(Mat<uchar> &image, Mat<uchar> &dst, double lowThresh, double highThresh) {
     dst = image;
-    int height = image.getHeight();
-    int width = image.getWidth();
-
+    uchar *data = dst.getData();
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
-            double gradValue = double(dst.ptr(i)[j]);
+            double gradValue = double((data + ptr(i))[j]);
             if (gradValue > highThresh) {
-                dst.ptr(i)[j] = (uchar)255;
+                (data + ptr(i))[j] = (uchar)255;
             } else if (gradValue < lowThresh) {
-                dst.ptr(i)[j] = (uchar)0;
+                (data + ptr(i))[j] = (uchar)0;
             }
         }
     }
@@ -120,16 +122,14 @@ void Canny::doubleThreshSelection(Mat<uchar> &image, Mat<uchar> &dst, double low
     this->edgeConnect(dst);
 }
 void Canny::edgeConnect(Mat<uchar> &image) {
-    int height = image.getHeight();
-    int width = image.getWidth();
-    
+    uchar *imageData = image.getData();
     for (int i = 1; i < height - 1; ++i) {
         for (int j = 1; j < width - 1; ++j) {
-            if (image.ptr(i)[j] == 255) {
+            if ((imageData + ptr(i))[j] == 255) {
                 for (int m = -1; m <= 1; ++m) {
                     for (int n = -1; n <= 1; ++n) {
-                        if (image.ptr(i + m)[j + n] != 0 && image.ptr(i +m)[j + n] != 255) {
-                            image.ptr(i + m)[j + n] = 255;
+                        if ((imageData + ptr(i + m))[j + n] != 0 && (imageData + ptr(i +m))[j + n] != 255) {
+                            (imageData + ptr(i + m))[j + n] = 255;
                         }
                     }
                 }
@@ -139,8 +139,8 @@ void Canny::edgeConnect(Mat<uchar> &image) {
     
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
-            if (image.ptr(i)[j] != 255) {
-                image.ptr(i)[j] = 0;
+            if ((imageData + ptr(i))[j] != 255) {
+                (imageData + ptr(i))[j] = 0;
             }
         }
     }
--- Main.cpp
+++ Main.cpp
@@ -2,52 +2,15 @@
 #include "Mat.h"
 #include <iostream>
 #include <fstream>
-#include "CImg-v.3.0.0/CImg.h"
 
 using namespace std;
-using namespace cimg_library;
 
 const double lowThresh = 70;
 const double highThresh = 200;
 const double gaussianSigma = 1;
+const int height = 512;
+const int width = 512;
 
-Mat<uchar> transformCimg2Mat(CImg<uchar> Gray_image) {
-    int width = Gray_image._width;
-    int height = Gray_image._height;
-    cout << width << height <<endl;
-    Mat<uchar> mat = Mat<uchar>::zeros(width, height);
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {    
-            mat.ptr(i)[j] =  Gray_image(j, i, 0);
-        }
-    }
-    return mat;
-}
-
-void transformMat2Cimg(CImg<uchar> &Gray_image, Mat<uchar> mat) {
-    cout << mat.getHeight() << mat.getWidth()<<endl;
-    for (int i = 0; i < mat.getHeight(); ++i) {
-        for (int j = 0; j < mat.getWidth(); ++j) {  
-            Gray_image(j, i, 0) = mat.ptr(i)[j];
-        }
-    }
-}
-
-CImg<uchar> getGrayImage(CImg<uchar> image) {
-    int width = image._width;
-    int height = image._height;
-    CImg<uchar> Gray_image(width, height, 1, 1, 0);
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            double r = image(j, i, 0);
-            double g = image(j, i, 1);
-            double b = image(j, i, 2);
-            Gray_image(j, i, 0) = 0.299*r + 0.587*g + 0.114*b;
-        }
-    }
-    return Gray_image;
-
-}
 Mat<uchar> readData(int width, int height) {
     Mat<uchar> mat = Mat<uchar>::zeros(width, height);
     int **a;
@@ -63,32 +26,16 @@ Mat<uchar> readData(int width, int height) {
         }
         fscanf(fp, "\n");
     }
-    cout <<"======"<<endl;
     return mat;
 }
+
 int main() {
-    /*CImg<uchar> image("lena1.bmp");
-    Canny *canny = new Canny();
-    CImg<uchar> Gray_image = getGrayImage(image);
-    Mat<uchar> mat = transformCimg2Mat(Gray_image);*/
-    Mat<uchar> Gray_image = readData(512, 512);
+    Mat<uchar> Gray_image = readData(height, width);
     Mat<uchar> gaussian_img = Gray_image;
     Canny *canny = new Canny();
-    //gaussian_img = canny->gaussianFilter(mat, gaussianSigma);
-    //transformMat2Cimg(Gray_image, gaussian_img);
-    //cout << Gray_image << endl;
-    //Gray_image.display("lena_out");
     Mat<uchar> grad, theta, dst, res;
 	canny->sobel(gaussian_img, grad, theta);
-    cout <<"===sobel==="<<endl;
 	canny->NMS(grad, theta, dst);
-    cout <<"===nms==="<<endl;
 	canny->doubleThreshSelection(dst, res, lowThresh, highThresh);
-    cout <<"===double==="<<endl;
-    CImg<uchar> resImage(512, 512, 1, 1, 0);
-    transformMat2Cimg(resImage, res);
-    resImage.display("lena_out");
-    resImage.save("lena_out.bmp");
-
     return 0;
 }
